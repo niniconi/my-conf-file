107a1a2cecd65b0b02acb989ac985dcc08026bc0
niniconi <Stump0828Provoking2393Y@gmail.com>
2022-11-12 23:19:56 +0800

    add install vim plug

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ git clone git@github.com:niniconi/my-conf-file.git
 cd my-conf-file
 echo $NEOVIM_PATH
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cp ./init.vim $NEOVIM_PATH
 
 cd ..
