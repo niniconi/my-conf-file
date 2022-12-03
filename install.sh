b85ab3065fd58d06ce6ae671693717824b4c7f6e
niniconi <Stump0828Provoking2393Y@gmail.com>
2022-12-03 20:47:51 +0800

    change install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-echo "user:"$HOME
+echo "[user]:"$HOME
 
-NEOVIM_PATH="${HOME}/.config/nvim/init.vim"
+NEOVIM_PATH="${HOME}/.config/nvim"
+PKG_LIST="git wget curl htop exa bottom neofetch"
+PKG_MGR="pacman -S"
 
-git clone git@github.com:niniconi/my-conf-file.git
+# install software
+sudo ${PKG_MGR} ${PKG_LIST}
 
-cd my-conf-file
-echo $NEOVIM_PATH
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-cp ./init.vim $NEOVIM_PATH
-
-cd ..
-rm my-conf-file -rf
+# vim config
+mkdir $NEOVIM_PATH -p
+rm $NEOVIM_PATH/init.vim
+wget -P $NEOVIM_PATH "https://raw.githubusercontent.com/niniconi/my-conf-file/master/init.vim"
