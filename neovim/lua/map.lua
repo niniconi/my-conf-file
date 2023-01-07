f90be6989547c44674dce8ec5d34b508ee62a916
niniconi <112842746+niniconi@users.noreply.github.com>
2023-01-08 00:35:40 +0800

    add lsp plugin ,edit map.lua

--- neovim/lua/map.lua
+++ neovim/lua/map.lua
@@ -1,4 +1,43 @@
+-- leader key 为空
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+local opt = {
+  noremap = true,
+  silent = true,
+}
+local pluginKeys = {}
 local map = vim.api.nvim_set_keymap
-map('n','<C-x>',':NERDTreeFocus<CR>',{noremap = true})
-map('n','<C-q>',':NERDTreeClose<CR>',{noremap = true})
-map('n','T'    ,':FloatermNew<CR>'  ,{noremap = true})
+map('n','<C-x>',':NERDTreeFocus<CR>',opt)
+map('n','<C-q>',':NERDTreeClose<CR>',opt)
+map('n','T'    ,':FloatermNew<CR>'  ,opt)
+
+--
+-- here is lsp's map conf
+--
+
+pluginKeys.maplsp = function(mapbuf)
+    -- rename
+    mapbuf('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opt)
+    -- code action
+    mapbuf('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opt)
+    -- go xx
+    mapbuf('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opt)
+    mapbuf('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opt)
+    mapbuf('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opt)
+    mapbuf('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opt)
+    mapbuf('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opt)
+    -- diagnostic
+    mapbuf('n', 'go', '<cmd>lua vim.diagnostic.open_float()<CR>', opt)
+    mapbuf('n', 'gp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opt)
+    mapbuf('n', 'gn', '<cmd>lua vim.diagnostic.goto_next()<CR>', opt)
+    -- mapbuf('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opt)
+    -- leader + =
+    mapbuf('n', '<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>', opt)
+    -- mapbuf('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opt)
+    -- mapbuf('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opt)
+    -- mapbuf('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opt)
+    -- mapbuf('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opt)
+    -- mapbuf('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opt)
+end
+
+return pluginKeys
--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -2,27 +2,27 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local paccker_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
-  vim.notify("Installing Pakcer.nvim ...")
-  paccker_bootstrap = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
+    vim.notify("Installing Pakcer.nvim ...")
+    paccker_bootstrap = fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    })
 
-  local rtp_addition = vim.fn.stdpath("data") .. "/site/pack/*/start/*"
-  if not string.find(vim.o.runtimepath, rtp_addition) then
-    vim.o.runtimepath = rtp_addition .. "," .. vim.o.runtimepath
-  end
-  vim.notify("Pakcer.nvim is installed")
+    local rtp_addition = vim.fn.stdpath("data") .. "/site/pack/*/start/*"
+    if not string.find(vim.o.runtimepath, rtp_addition) then
+        vim.o.runtimepath = rtp_addition .. "," .. vim.o.runtimepath
+    end
+    vim.notify("Pakcer.nvim is installed")
 end
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  vim.notify("not install packer.nvim")
-  return
+    vim.notify("not install packer.nvim")
+    return
 end
 
 packer.startup({
@@ -38,7 +38,11 @@ packer.startup({
         use("Xuyuanp/nerdtree-git-plugin") --git for NERDTree
         use("mhinz/vim-startify")
         use("voldikss/vim-floaterm") --terminal
-
+        --lsp
+        use {
+            "williamboman/nvim-lsp-installer",
+            "neovim/nvim-lspconfig",
+        }
         --colorcheme
         -- use("morhetz/gruvbox")
         use("joshdick/onedark.vim")
@@ -73,3 +77,6 @@ packer.startup({
 -- import plugins conf
 require("plugins.powerline")
 require("plugins.vim-startify")
+
+-- import lsp
+require("lsp.setup")
