5e7f9fc9253da396866e5ea28773c37f5486c72a
niniconi <112842746+niniconi@users.noreply.github.com>
2023-01-14 22:16:05 +0800

    change ranger/install.sh

--- ranger/install.sh
+++ ranger/install.sh
@@ -3,7 +3,7 @@ mkdir ~/.config/ranger -p
 cp ./rc.conf ~/.config/ranger/
 cp ./rifle.conf ~/.config/ranger/
 
-if [ ! -d "~/.config/ranger/plugins/ranger_devicons" ];then
+if [ ! -d ~/.config/ranger/plugins/ranger_devicons ];then
     echo -e "${installing}:ranger devicons"
     git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 fi
