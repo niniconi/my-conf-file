dc718e4fa4f34f84f9c1a12606c272b7d4435155
niniconi <112842746+niniconi@users.noreply.github.com>
2023-01-14 15:39:29 +0800

    add a new ranger plugin

--- ranger/install.sh
+++ ranger/install.sh
@@ -2,3 +2,8 @@
 mkdir ~/.config/ranger -p
 cp ./rc.conf ~/.config/ranger/
 cp ./rifle.conf ~/.config/ranger/
+
+if [ ! -d "~/.config/ranger/plugins/ranger_devicons" ];then
+    echo -e "${installing}:ranger devicons"
+    git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+fi
