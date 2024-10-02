a3acb272926d7aae42796700b4b71507a43c7ecd
niniconi <Stump0828Provoking2393Y@gmail.com>
2024-10-02 19:06:33 +0800

    add hyprland configuration

---
+++ hypr/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+execute_exist Hyprland hyprpaper
+install_config_file "${CONFIG_PATH}/hyprland.conf" "$HOME/.config/hypr"
+install_config_dir "${CONFIG_PATH}/hyprpaper.conf" "$HOME/.config/hypr"
